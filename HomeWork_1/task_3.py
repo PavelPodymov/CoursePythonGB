1020ba6b6d9bd5dad5c03acd393472d49142aa70
PavelPodymov <poll.ppbpower@gmail.com>
2022-11-22 21:00:04 +0300

    Task №3 done

---
+++ HomeWork_1/task_3.py
@@ -0,0 +1,14 @@
+"""
+Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
+Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
+"""
+# CONSTANT
+NUMBER = ''
+ANSWER = 0
+
+user_number = input("Please input your number: ")
+
+for i in range(3):
+    NUMBER += user_number
+    ANSWER += int(NUMBER)
+print(ANSWER)
