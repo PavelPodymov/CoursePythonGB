2fe975feee55fdab241e43f20917066ca7168910
PavelPodymov <poll.ppbpower@gmail.com>
2022-11-23 11:47:08 +0300

    Task №5 done

---
+++ HomeWork_1/task_5.py
@@ -0,0 +1,20 @@
+"""
+Запросите у пользователя значения выручки и издержек фирмы. Определите,
+с каким финансовым результатом работает фирма (прибыль — выручка больше издержек,
+или убыток — издержки больше выручки). Выведите соответствующее сообщение.
+Если фирма отработала с прибылью, вычислите рентабельность выручки
+(соотношение прибыли к выручке). Далее запросите численность сотрудников фирмы и определите
+прибыль фирмы в расчете на одного сотрудника.
+"""
+
+proceeds = int(input("Please input your proceeds, rub: "))
+costs = int(input("Please input your costs, rub: "))
+
+income = proceeds - costs
+
+if proceeds > costs:
+    print(f"Your income = {income}, you are doing great!")
+    number_of_staff = int(input("Please input number of staff: "))
+    print(f"Your income for one person is {round(income / number_of_staff, 2)} rub.")
+else:
+    print(f"Your income = {income}, your scores are not very good!")
