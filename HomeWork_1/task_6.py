97df8a7f481b98d5af3a56e409afb7da7643d2b0
PavelPodymov <poll.ppbpower@gmail.com>
2022-11-23 11:20:41 +0300

    Task №6 done

---
+++ HomeWork_1/task_6.py
@@ -0,0 +1,34 @@
+"""
+Спортсмен занимается ежедневными пробежками. В первый день его результат составил a километров.
+Каждый день спортсмен увеличивал результат на 10 % относительно предыдущего.
+Требуется определить номер дня, на который результат спортсмена составит не менее b километров.
+Программа должна принимать значения параметров a и b и
+выводить одно натуральное число — номер дня.
+
+Например: a = 2, b = 3.
+Результат:
+1-й день: 2
+2-й день: 2,2
+3-й день: 2,42
+4-й день: 2,66
+5-й день: 2,93
+6-й день: 3,22
+Ответ: на 6-й день спортсмен достиг результата — не менее 3 км.
+"""
+
+
+def find_day_when_reach_goal(first_day, goal):
+    """find the day when sportsmen reach the goal"""
+    count = 1
+    ans = first_day
+    while goal > ans:
+        print(f"{count}-й день: {round(ans, 2)}")
+        ans *= 1.1
+        count += 1
+    print(f"{count}-й день: {round(ans, 2)}")
+    print(f"Answer: on the {count} days sportsman reach goal - more than {goal} km.")
+
+
+a = int(input("First day jogging km = "))
+b = int(input("Goal km = "))
+find_day_when_reach_goal(a, b)
