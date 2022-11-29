452f820660cf97d8dcc7e2a12085eae48a2dca3f
PavelPodymov <poll.ppbpower@gmail.com>
2022-11-29 23:48:37 +0300

    Task 2 done

---
+++ HomeWork_3/task_2.py
@@ -0,0 +1,21 @@
+"""
+Реализовать функцию, принимающую несколько параметров, описывающих
+данные пользователя: имя, фамилия, год рождения, город проживания, email,
+телефон. Функция должна принимать параметры как именованные аргументы.
+Реализовать вывод данных о пользователе одной строкой.
+"""
+
+
+def personal_description(**kwargs):
+    """Function take information from person"""
+    return kwargs
+
+
+print(personal_description(
+    name='Pavel',
+    secname='Podymov',
+    year=1982,
+    city='Lubertsy',
+    email='erer@gmail.com',
+    phone=123456789
+))
