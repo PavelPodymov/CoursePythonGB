9ef2ddf5240062322ec95aa09ea1f8427c737484
PavelPodymov <poll.ppbpower@gmail.com>
2022-12-01 13:39:23 +0300

    Extra task 4 done

---
+++ HomeWork_3/extra_example_4.py
@@ -0,0 +1,19 @@
+"""
+Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+Пример:
+- 45 -> 101101
+- 3 -> 11
+- 2 -> 10
+"""
+
+
+def dec_bin(some_n, result=''):
+    """convert number to binary number"""
+    if some_n != 0:
+        result += dec_bin(some_n // 2, result) + str(some_n % 2)
+    return result
+
+
+print(dec_bin(45))
+print(dec_bin(3))
+print(dec_bin(2))
