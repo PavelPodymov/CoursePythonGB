d7ea8f69cd429e6c2da604097c88e32155aebf90
PavelPodymov <poll.ppbpower@gmail.com>
2022-12-12 11:52:48 +0300

    HomeWork 6 done

--- HomeWork_6_1/code_4_testing.py
+++
@@ -1,7 +0,0 @@
-from timeit import timeit
-
-print(timeit("fact_list", globals=globals()))
-print(timeit("fact_diction", globals=globals()))
-print(timeit("fact_math", globals=globals()))
-
-# отработку функций по памяти и скорости смотри файл code_4.md
