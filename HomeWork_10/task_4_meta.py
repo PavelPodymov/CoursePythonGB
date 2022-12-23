ed9f8b2deaf5632da7fcad811bdbafb337aacf97
PavelPodymov <poll.ppbpower@gmail.com>
2022-12-23 16:10:57 +0300

    Homework 10 Done

---
+++ HomeWork_10/task_4_meta.py
@@ -0,0 +1,24 @@
+"""MetaClass"""
+
+
+class MyMetaClass(type):
+    """MetaClass for new class"""
+    _instance = None
+
+    def __new__(cls, name, bases, dct):
+        """new"""
+        if cls._instance is None:
+            cls._instance = super(MyMetaClass, cls).__new__(cls, name, bases, dct)
+        else:
+            return cls._instance
+
+
+class Test(metaclass=MyMetaClass):
+    """class Test"""
+    pass
+
+
+TestObj = Test
+TestObject = Test
+TestObjects = Test
+print(TestObj is TestObject is TestObjects)
