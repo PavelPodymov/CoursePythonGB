82bb93f49a0e77072a9af8023422fdb10d51c61f
PavelPodymov <poll.ppbpower@gmail.com>
2022-12-23 21:39:42 +0300

    Homework 10 fix task 4 again

--- HomeWork_10/task_4_meta.py
+++ HomeWork_10/task_4_meta.py
@@ -2,27 +2,22 @@
 
 
 class MyMetaClass(type):
-    """MetaClass for new class"""
     _instance = None
 
-    def __new__(cls, name, bases, dct):
-        """new"""
+    def __call__(cls, *args, **kwargs):
         if cls._instance is None:
-            cls._instance = super().__new__(cls, name, bases, dct)
-        else:
-            return cls._instance
+            cls._instance = super().__call__(*args, **kwargs)
+        return cls._instance
 
 
 class Test(metaclass=MyMetaClass):
-    """class Test"""
     pass
 
 
-TestObj = Test
-TestObject = Test
-TestObjects = Test
+TestObj = Test()
+TestObject = Test()
+TestObjects = Test()
 print(TestObj is TestObject is TestObjects)
-
 
 
 
